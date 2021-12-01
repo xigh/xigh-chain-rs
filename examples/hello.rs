38fb99b207b43042bf823f157c53e14c7459351d
Philippe Anel <zexigh@gmail.com>
2021-12-01 21:17:13 +0100

    not a coin

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,22 +1,22 @@
 extern crate xigh_chain_rs;
 
 fn main() {
-    let xighcoin = &mut xigh_chain_rs::Blockchain::new();
+    let xc = &mut xigh_chain_rs::Blockchain::new();
     
     println!("add vec 1:");
-    if !xighcoin.add(vec![0, 1, 2]) {
+    if !xc.add(vec![0, 1, 2]) {
         panic!("could not add vec 1");
     }
 
     println!("add vec 2:");
-    if !xighcoin.add(vec![3, 4, 5, 6]) {
+    if !xc.add(vec![3, 4, 5, 6]) {
         panic!("could not add vec 1");
     }
     
     println!("dump chain:");
-    xighcoin.dump_chain();
+    xc.dump_chain();
     
     println!("trying to tamper data:");
-    xighcoin.tamper_data(1, vec!(7, 8));
-    println!("is_valid {}", xighcoin.is_valid());
+    xc.tamper_data(1, vec!(7, 8));
+    println!("is_valid {}", xc.is_valid());
 }
