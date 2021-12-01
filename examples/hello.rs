de2a098e1a1d01b5c194e481b0017f9c6771d066
Philippe Anel <zexigh@gmail.com>
2021-12-01 21:11:02 +0100

    text cleanup

--- examples/hello.rs
+++ examples/hello.rs
@@ -3,15 +3,17 @@ extern crate xigh_chain_rs;
 fn main() {
     let xighcoin = &mut xigh_chain_rs::Blockchain::new();
     
-    println!("add vecs:");
+    println!("add vec 1:");
     if !xighcoin.add(vec![0, 1, 2]) {
         panic!("could not add vec 1");
     }
 
+    println!("add vec 2:");
     if !xighcoin.add(vec![3, 4, 5, 6]) {
         panic!("could not add vec 1");
     }
     
+    println!("dump chain:");
     xighcoin.dump_chain();
     
     println!("trying to tamper data:");
